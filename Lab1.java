eb71a7448f2bace5ea0aaed9cdec66abd128e2b6
Shishkov Egor <131530870+AnimeMakartur@users.noreply.github.com>
2026-09-10 22:02:48 +0300

    Add normal Docs and split clases

--- Lab1.java
+++ Lab1.java
@@ -1,55 +1,36 @@
 import java.util.Scanner;
-
-class Lucas {
-    private int index;
-    private long lucasValue;
-    public Lucas(int index) {
-        this.index = index;
-        this.lucasValue = calculateLucas(index);
-    }
-    public long getLucasValue() {
-        return lucasValue;
-    }
-    public int getIndex() {
-        return index;
-    }
-    public long calculateLucas(int n) {
-        if (n == 0) {
-            return 2;
-        } else if (n == 1) {
-
-            return 1;
-
-        }  else {
-            return calculateLucas(n - 1) + calculateLucas(n - 2);
-        }
-        
-    }
-    public boolean checkInequality() {
-        long leftSide = index * calculateLucas(index + 1);
-        long rightSide = (index + 1) * calculateLucas(index);
-        return leftSide > rightSide;
-    }
-}
-
+/**
+ * Головний клас програми. Приймає номер n або через командний рядок,
+ * або з клавіатури, обчислює число Люка та перевіряє нерівність.
+ *
+ * @author Шишков Єгор ОІ-21
+ */
 public class Lab1 {
+    /**
+     * Точка входу в програму.
+     *
+     * @param args аргументи командного рядка; якщо передано один аргумент,
+     *             його трактують як номер n
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 0;
+        int n;
 
         if (args.length == 1) {
             n = Integer.parseInt(args[0]);
         } else {
-            System.out.println("Please input index for Lucas number:");
+            System.out.print("Please input index for Lucas number: ");
             n = scanner.nextInt();
         }
+
         if (n < 0) {
             System.out.println("Index must be non-negative");
             scanner.close();
             return;
         }
+
         Lucas lucas = new Lucas(n);
-        
+
         System.out.println("Lucas number details:");
         System.out.println("n = " + lucas.getIndex());
         System.out.println("L(" + lucas.getIndex() + ") = " + lucas.getLucasValue());
@@ -63,6 +44,7 @@ public class Lab1 {
             System.out.println(lucas.getIndex() + " * L(" + (lucas.getIndex() + 1) + ") <= "
                     + (lucas.getIndex() + 1) + " * L(" + lucas.getIndex() + ")");
         }
+
         scanner.close();
     }
 }
---
+++ Lucas.java
@@ -0,0 +1,72 @@
+/**
+ * Клас {@code Lucas} використовується для зберігання номера n
+ * та значення числа Люка L(n). Клас також дозволяє обчислювати
+ * число Люка і перевіряти виконання нерівності
+ * n * L(n + 1) > (n + 1) * L(n).
+ *
+ * @author Шишков Єгор ОІ-21
+ */
+public class Lucas {
+    private final int index;
+    private final long lucasValue;
+
+    /**
+     * Конструктор класу {@code Lucas}.
+     *
+     * @param index номер числа Люка
+     * @throws IllegalArgumentException якщо індекс є від'ємним
+     */
+    public Lucas(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Індекс числа Люка не може бути від'ємним.");
+        }
+        this.index = index;
+        this.lucasValue = calculateLucas(index);
+    }
+
+    /**
+     * Повертає номер числа Люка.
+     *
+     * @return значення поля {@code index}
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    /**
+     * Повертає значення числа Люка для поточного індексу.
+     *
+     * @return значення поля {@code lucasValue}
+     */
+    public long getLucasValue() {
+        return lucasValue;
+    }
+
+    /**
+     * Обчислює n-те число Люка рекурсивно.
+     *
+     * @param n номер числа Люка
+     * @return значення L(n)
+     */
+    public long calculateLucas(int n) {
+        if (n == 0) {
+            return 2;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return calculateLucas(n - 1) + calculateLucas(n - 2);
+        }
+    }
+
+    /**
+     * Перевіряє виконання нерівності:
+     * n * L(n + 1) > (n + 1) * L(n).
+     *
+     * @return {@code true}, якщо нерівність виконується, інакше {@code false}
+     */
+    public boolean checkInequality() {
+        long leftSide = (long) index * calculateLucas(index + 1);
+        long rightSide = (long) (index + 1) * lucasValue;
+        return leftSide > rightSide;
+    }
+}
